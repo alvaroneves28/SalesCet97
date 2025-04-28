f8feac300deb7c2d437c7a8e512a3b94c4a52bdb
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-04-28 10:42:37 +0100

    create database and countries table

---
+++ SalesCet97.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCet97.Web.Data.Entities;
+
+namespace SalesCet97.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option) 
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+      
+    }
+}
